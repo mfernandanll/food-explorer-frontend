ab9f2af3f9afa3b0b1c7999c2b4dca6e1c6e6e9e
Fernanda Loureiro <m.fernanda.nll@gmail.com>
2024-08-19 16:21:21 -0400

    Changing api bareurl to backend deploy url

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,5 @@
 import axios from "axios";
 
 export const api = axios.create({
-  // baseURL: 'https://food-explorer-backend-qs1l.onrender.com'
-  baseURL: 'http://localhost:3333'
+  baseURL: 'https://food-explorer-backend-qs1l.onrender.com'
 });
